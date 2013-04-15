23e0b36c8eac6379330e9b6ff5703043e4b399ec
pocha <pocha@stalkninja.com>
2013-04-15 17:25:34 +0530

    updated initial dump with categories. other items are reset

--- initial_dump_with_categories.sql
+++ initial_dump_with_categories.sql
@@ -206,7 +206,6 @@ CREATE TABLE `qa_iplimits` (
 
 LOCK TABLES `qa_iplimits` WRITE;
 /*!40000 ALTER TABLE `qa_iplimits` DISABLE KEYS */;
-INSERT INTO `qa_iplimits` VALUES (2130706433,'A',379304,3),(2130706433,'Q',379304,7);
 /*!40000 ALTER TABLE `qa_iplimits` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -230,7 +229,7 @@ CREATE TABLE `qa_options` (
 
 LOCK TABLES `qa_options` WRITE;
 /*!40000 ALTER TABLE `qa_options` DISABLE KEYS */;
-INSERT INTO `qa_options` VALUES ('allow_close_questions','1'),('allow_multi_answers','1'),('allow_no_category',''),('allow_no_sub_category',''),('allow_self_answer','1'),('avatar_q_list_size','0'),('avatar_q_page_a_size','40'),('avatar_q_page_q_size','50'),('avatar_users_size','30'),('block_bad_words',''),('block_ips_write',''),('cache_acount','3'),('cache_ccount','0'),('cache_qcount','13'),('cache_tagcount','0'),('cache_unaqcount','12'),('cache_unselqcount','13'),('cache_unupaqcount','13'),('cache_userpointscount','1'),('captcha_module','reCAPTCHA'),('captcha_on_anon_post','1'),('columns_users','2'),('comment_on_as','1'),('comment_on_qs','0'),('custom_answer',''),('custom_ask',''),('custom_comment',''),('custom_footer',''),('custom_header',''),('custom_home_content',''),('custom_home_heading',''),('custom_in_head',''),('custom_sidepanel',''),('db_version','47'),('do_ask_check_qs','0'),('do_close_on_select','0'),('do_complete_tags','1'),('do_count_q_views','1'),('do_example_tags','1'),('editor_for_as','Markdown Editor'),('editor_for_cs',''),('editor_for_qs','Markdown Editor'),('email_privacy','Privacy: Your email address will not be shared or sold to third parties.'),('event_logger_to_database',''),('event_logger_to_files',''),('extra_field_active','0'),('extra_field_display','0'),('extra_field_label',''),('extra_field_prompt',''),('feedback_email','devs@codelearn.org'),('feedback_enabled','1'),('feed_for_hot',''),('feed_for_qa','1'),('feed_for_questions','1'),('feed_for_unanswered','1'),('feed_per_category','1'),('flagging_of_posts','1'),('follow_on_as','1'),('from_email','founders@codelearn.org'),('home_description',''),('hot_weight_answers','100'),('hot_weight_a_age','100'),('hot_weight_q_age','100'),('hot_weight_views','100'),('hot_weight_votes','100'),('links_in_new_window',''),('logo_height',''),('logo_show',''),('logo_url',''),('logo_width',''),('markdown_editor_css',''),('markdown_highlightjs',''),('match_ask_check_qs','3'),('max_len_q_title','120'),('max_num_q_tags','5'),('max_rate_ip_as','50'),('max_rate_ip_qs','20'),('max_rate_user_as','25'),('max_rate_user_qs','10'),('max_store_user_updates','50'),('min_len_a_content','12'),('min_len_c_content','12'),('min_len_q_content','0'),('min_len_q_title','12'),('min_num_q_tags','0'),('mouseover_content_on',''),('nav_activity','0'),('nav_ask','1'),('nav_categories','1'),('nav_home',''),('nav_hot','0'),('nav_qa_is_home','0'),('nav_questions','1'),('nav_tags','1'),('nav_unanswered','0'),('nav_users','1'),('neat_urls','1'),('notify_admin_q_post','1'),('notify_users_default','1'),('pages_prev_next','3'),('page_size_activity','20'),('page_size_ask_check_qs','5'),('page_size_ask_tags','5'),('page_size_home','20'),('page_size_hot_qs','20'),('page_size_qs','20'),('page_size_q_as','10'),('page_size_search','10'),('page_size_una_qs','20'),('page_size_users','20'),('page_size_user_posts','20'),('permit_anon_view_ips','70'),('permit_close_q','70'),('permit_delete_hidden','40'),('permit_edit_a','100'),('permit_edit_q','70'),('permit_flag','110'),('permit_hide_show','70'),('permit_moderate','100'),('permit_post_a','150'),('permit_post_c','150'),('permit_post_q','150'),('permit_retag_cat','70'),('permit_select_a','100'),('permit_view_q_page','150'),('permit_vote_a','120'),('permit_vote_down','120'),('permit_vote_q','120'),('points_a_selected','30'),('points_a_voted_max_gain','20'),('points_a_voted_max_loss','5'),('points_base','100'),('points_multiple','10'),('points_per_a_voted',''),('points_per_a_voted_down','2'),('points_per_a_voted_up','2'),('points_per_q_voted',''),('points_per_q_voted_down','1'),('points_per_q_voted_up','1'),('points_post_a','4'),('points_post_q','2'),('points_q_voted_max_gain','10'),('points_q_voted_max_loss','3'),('points_select_a','3'),('points_to_titles',''),('points_vote_down_a','1'),('points_vote_down_q','1'),('points_vote_on_a',''),('points_vote_on_q',''),('points_vote_up_a','1'),('points_vote_up_q','1'),('q_urls_remove_accents',''),('q_urls_title_length','50'),('recaptcha_public_key',''),('search_module',''),('show_a_c_links','1'),('show_a_form_immediate','if_no_as'),('show_custom_answer','0'),('show_custom_ask','0'),('show_custom_comment','0'),('show_custom_footer',''),('show_custom_header',''),('show_custom_home',''),('show_custom_in_head',''),('show_custom_sidebar','1'),('show_custom_sidepanel',''),('show_c_reply_buttons','1'),('show_fewer_cs_count','5'),('show_fewer_cs_from','10'),('show_full_date_days','7'),('show_home_description',''),('show_notice_visitor',''),('show_selected_first','1'),('show_url_links','1'),('show_user_points','1'),('show_user_titles','1'),('show_view_counts',''),('show_when_created','1'),('site_language',''),('site_maintenance','0'),('site_theme','Snow'),('site_theme_mobile','Snow'),('site_title','Codelearn Forum'),('site_url','http://localhost/qa/'),('smtp_active','1'),('smtp_address','email-smtp.us-east-1.amazonaws.com'),('smtp_authenticate','1'),('smtp_password','ApNdhFQHEbzZvfC4unmW6ceIWP2rz9OLMG4VWaWzqkhe'),('smtp_port','587'),('smtp_secure','tls'),('smtp_username','AKIAJZHIRPXOWZQFLOGQ'),('sort_answers_by','created'),('tags_or_categories','c'),('tag_separator_comma',''),('votes_separated',''),('voting_on_as','1'),('voting_on_qs','1'),('voting_on_q_page_only',''),('wysiwyg_editor_upload_images','');
+INSERT INTO `qa_options` VALUES ('block_ips_write',''),('cache_tagcount',''),('custom_footer',''),('custom_header',''),('custom_in_head',''),('db_version','47'),('event_logger_to_database',''),('feedback_enabled','1'),('flagging_of_posts','1'),('logo_height',''),('logo_show',''),('logo_url',''),('logo_width',''),('nav_activity',''),('nav_ask','1'),('nav_categories',''),('nav_home',''),('nav_hot',''),('nav_qa_is_home',''),('nav_questions','1'),('nav_tags','1'),('nav_unanswered','1'),('nav_users','1'),('neat_urls','5'),('page_size_home','20'),('permit_hide_show','70'),('permit_moderate','100'),('permit_post_q','150'),('show_custom_footer',''),('show_custom_header',''),('show_custom_home',''),('show_custom_in_head',''),('show_custom_sidebar','1'),('show_custom_sidepanel',''),('show_notice_visitor',''),('site_language',''),('site_maintenance',''),('site_theme','Snow'),('site_theme_mobile','Snow'),('site_url','http://localhost/qa/'),('tags_or_categories','tc');
 /*!40000 ALTER TABLE `qa_options` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -357,7 +356,7 @@ CREATE TABLE `qa_posts` (
   CONSTRAINT `qa_posts_ibfk_2` FOREIGN KEY (`parentid`) REFERENCES `qa_posts` (`postid`),
   CONSTRAINT `qa_posts_ibfk_3` FOREIGN KEY (`categoryid`) REFERENCES `qa_categories` (`categoryid`) ON DELETE SET NULL,
   CONSTRAINT `qa_posts_ibfk_4` FOREIGN KEY (`closedbyid`) REFERENCES `qa_posts` (`postid`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -422,7 +421,6 @@ CREATE TABLE `qa_sharedevents` (
 
 LOCK TABLES `qa_sharedevents` WRITE;
 /*!40000 ALTER TABLE `qa_sharedevents` DISABLE KEYS */;
-INSERT INTO `qa_sharedevents` VALUES ('Q',1,1,1,NULL,2,'2013-04-08 23:34:03'),('C',1,1,1,NULL,2,'2013-04-08 23:34:03'),('C',2,1,1,NULL,2,'2013-04-08 23:34:03'),('Q',2,2,2,NULL,2,'2013-04-09 10:09:50'),('C',7,2,2,NULL,2,'2013-04-09 10:09:50'),('C',8,2,2,NULL,2,'2013-04-09 10:09:50'),('Q',3,3,3,NULL,2,'2013-04-09 12:25:28'),('C',1,3,3,NULL,2,'2013-04-09 12:25:28'),('C',3,3,3,NULL,2,'2013-04-09 12:25:28'),('Q',4,4,4,NULL,2,'2013-04-09 12:47:51'),('C',7,4,4,NULL,2,'2013-04-09 12:47:51'),('C',8,4,4,NULL,2,'2013-04-09 12:47:51'),('Q',5,5,5,NULL,2,'2013-04-09 13:05:59'),('C',1,5,5,NULL,2,'2013-04-09 13:05:59'),('C',4,5,5,NULL,2,'2013-04-09 13:05:59'),('Q',6,6,6,NULL,2,'2013-04-09 13:21:00'),('C',1,6,6,NULL,2,'2013-04-09 13:21:00'),('C',2,6,6,NULL,2,'2013-04-09 13:21:00'),('Q',7,7,7,NULL,2,'2013-04-09 13:36:26'),('C',13,7,7,NULL,2,'2013-04-09 13:36:26'),('C',14,7,7,NULL,2,'2013-04-09 13:36:26'),('Q',7,7,8,NULL,2,'2013-04-09 13:38:26'),('Q',7,7,9,NULL,2,'2013-04-09 13:39:50'),('Q',7,7,10,NULL,2,'2013-04-09 13:41:12'),('Q',11,11,11,NULL,2,'2013-04-09 13:41:26'),('C',13,11,11,NULL,2,'2013-04-09 13:41:26'),('C',14,11,11,NULL,2,'2013-04-09 13:41:26'),('Q',13,13,13,NULL,2,'2013-04-09 13:42:44'),('C',13,13,13,NULL,2,'2013-04-09 13:42:44'),('C',14,13,13,NULL,2,'2013-04-09 13:42:44'),('Q',14,14,14,NULL,2,'2013-04-09 13:44:37'),('C',13,14,14,NULL,2,'2013-04-09 13:44:37'),('C',14,14,14,NULL,2,'2013-04-09 13:44:37'),('Q',15,15,15,NULL,2,'2013-04-09 13:48:55'),('C',13,15,15,NULL,2,'2013-04-09 13:48:55'),('C',14,15,15,NULL,2,'2013-04-09 13:48:55'),('Q',16,16,16,NULL,2,'2013-04-09 13:56:22'),('C',1,16,16,NULL,2,'2013-04-09 13:56:22'),('C',4,16,16,NULL,2,'2013-04-09 13:56:22');
 /*!40000 ALTER TABLE `qa_sharedevents` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -581,7 +579,6 @@ CREATE TABLE `qa_userlimits` (
 
 LOCK TABLES `qa_userlimits` WRITE;
 /*!40000 ALTER TABLE `qa_userlimits` DISABLE KEYS */;
-INSERT INTO `qa_userlimits` VALUES (2,'A',379304,3),(2,'Q',379304,7);
 /*!40000 ALTER TABLE `qa_userlimits` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -672,7 +669,6 @@ CREATE TABLE `qa_userpoints` (
 
 LOCK TABLES `qa_userpoints` WRITE;
 /*!40000 ALTER TABLE `qa_userpoints` DISABLE KEYS */;
-INSERT INTO `qa_userpoints` VALUES (2,480,13,3,0,0,0,0,0,0,0,0,0,0,0,0);
 /*!40000 ALTER TABLE `qa_userpoints` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -747,7 +743,7 @@ CREATE TABLE `qa_words` (
   PRIMARY KEY (`wordid`),
   KEY `word` (`word`),
   KEY `tagcount` (`tagcount`)
-) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -756,7 +752,6 @@ CREATE TABLE `qa_words` (
 
 LOCK TABLES `qa_words` WRITE;
 /*!40000 ALTER TABLE `qa_words` DISABLE KEYS */;
-INSERT INTO `qa_words` VALUES (1,'this',3,6,0,0),(2,'is',1,3,0,0),(3,'sample',2,2,0,0),(4,'question',6,3,0,0),(5,'content',0,1,0,0),(6,'title',1,0,0,0),(7,'given',1,0,0,0),(8,'one',4,0,0,0),(9,'more',3,0,0,0),(10,'my',0,1,0,0),(11,'name',0,1,0,0),(12,'ashish',0,1,0,0),(13,'sharma',0,1,0,0),(14,'enter',0,1,0,0),(15,'code',0,1,0,0),(16,'ye',0,1,0,0),(17,'comes',1,1,0,0),(18,'as',0,1,0,0),(19,'nice',0,1,0,0),(20,'m1',1,0,0,0),(21,'l2',1,0,0,0),(22,'empty',0,1,0,0),(23,'def',0,1,0,0),(24,'pocha',0,1,0,0),(25,'var',0,1,0,0),(26,'a',1,1,0,0),(27,'2',0,1,0,0),(28,'1',0,1,0,0),(29,'hola',0,1,0,0),(30,'questions',1,0,0,0),(31,'should',1,2,0,0),(32,'send',1,2,0,0),(33,'email',3,3,0,0),(34,'to',1,1,0,0),(35,'devs@codelearn',0,1,0,0),(36,'org',0,1,0,0),(37,'testing',2,0,0,0),(38,'hope',1,0,0,0),(39,'goes',2,0,0,0),(40,'now',1,0,0,0),(41,'go',0,1,0,0),(42,'another',2,1,0,0),(43,'test',1,1,0,0),(44,'q',0,1,0,0),(45,'answer',0,2,0,0),(46,'ok',1,1,0,0),(47,'shuld',0,1,0,0),(48,'not',0,1,0,0),(49,'be',0,1,0,0),(50,'duplicate',0,1,0,0),(51,'askign',1,0,0,0),(52,'new',2,0,0,0),(53,'time',3,0,0,0),(54,'trying',2,0,0,0),(55,'here',1,0,0,0),(56,'it',2,0,0,0),(57,'taking',1,0,0,0),(58,'lets',1,0,0,0),(59,'see',1,0,0,0),(60,'if',1,0,0,0),(61,'amazon',1,0,0,0),(62,'emails',1,0,0,0),(63,'working',1,0,0,0),(64,'last',1,0,0,0);
 /*!40000 ALTER TABLE `qa_words` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -769,4 +764,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-04-12 16:47:36
+-- Dump completed on 2013-04-15 17:25:17
