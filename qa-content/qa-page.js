80f345191544aaa6fda64967bc27a200f5f76516
pocha <pocha@stalkninja.com>
2013-04-22 17:49:42 +0530

    added trackpage view code which was missing because of Google cant track Q&A access

--- qa-content/qa-page.js
+++ qa-content/qa-page.js
@@ -166,4 +166,4 @@ _gaq.push(['_setDomainName', 'codelearn.org']);
 	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
-
+_gaq.push(['_trackPageview']);
